597fc07a06d4b52038ddf650e908a28d7cfa8db2
fengrongheng <fengrongheng@rrslj.com>
2016-04-25 16:19:57 +0800

    Initial Commit

---
+++ TabbarViewControllerText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabbarViewControllerText
+//
+//  Created by Rongheng on 16/4/25.
+//  Copyright © 2016年 SY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
